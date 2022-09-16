343bf6026bc162630add7eb043a7cce4edb2177d
wiasliaw <wiasliaw@protonmail.com>
2022-09-16 12:05:57 +0800

    feat: add multicall

--- multicall-contract/build.rs
+++ multicall-contract/build.rs
@@ -4,9 +4,14 @@ use ethers::contract::Abigen;
 
 fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("multicall2.rs");
 
+    let dest_path = Path::new(&out_dir).join("multicall2.rs");
     Abigen::new("Multicall2", "./src/multicall2_abi.json").unwrap()
+        .generate().unwrap()
+        .write_to_file(dest_path).unwrap();
+
+    let dest_path = Path::new(&out_dir).join("multicall.rs");
+    Abigen::new("Multicall", "./src/multicall_abi.json").unwrap()
         .generate().unwrap()
         .write_to_file(dest_path).unwrap();
 
--- multicall-contract/src/lib.rs
+++ multicall-contract/src/lib.rs
@@ -1,3 +1,5 @@
+include!(concat!(env!("OUT_DIR"), "/multicall.rs"));
 include!(concat!(env!("OUT_DIR"), "/multicall2.rs"));
 
+pub use multicall as multicall1;
 pub use multicall_2 as multicall2;
