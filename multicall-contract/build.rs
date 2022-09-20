f0d752ec3a8c7722f6032d387cbefe4602cdd381
wiasliaw <wiasliaw@protonmail.com>
2022-09-20 12:06:08 +0800

    chore: fix style

--- multicall-contract/build.rs
+++ multicall-contract/build.rs
@@ -1,19 +1,25 @@
+use ethers::contract::Abigen;
 use std::env;
 use std::path::Path;
-use ethers::contract::Abigen;
 
 fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
 
     let dest_path = Path::new(&out_dir).join("multicall2.rs");
-    Abigen::new("Multicall2", "./src/multicall2_abi.json").unwrap()
-        .generate().unwrap()
-        .write_to_file(dest_path).unwrap();
+    Abigen::new("Multicall2", "./src/multicall2_abi.json")
+        .unwrap()
+        .generate()
+        .unwrap()
+        .write_to_file(dest_path)
+        .unwrap();
 
     let dest_path = Path::new(&out_dir).join("multicall.rs");
-    Abigen::new("Multicall", "./src/multicall_abi.json").unwrap()
-        .generate().unwrap()
-        .write_to_file(dest_path).unwrap();
+    Abigen::new("Multicall", "./src/multicall_abi.json")
+        .unwrap()
+        .generate()
+        .unwrap()
+        .write_to_file(dest_path)
+        .unwrap();
 
     println!("cargo:rerun-if-changed=build.rs");
 }
